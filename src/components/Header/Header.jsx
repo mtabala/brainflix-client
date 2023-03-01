b74f7ccb0127e76dd401886bf9b885b5c4f80713
Masha <mariia.tabala@gmail.com>
2023-02-28 21:44:31 -0800

    Create a draft of the header for mobile start basic styling

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import "./Header.scss"
+import logo from "../../assets/Logo/BrainFlix-logo.svg"
+import mohanImg from "../../assets/Images/Mohan-muruge.jpg"
+
+function Header () {
+    return (
+        <>
+        <header className="header">
+            <a className="header__logo">
+                <img className="header__logo-img" src={logo} alt="logo" />
+            </a>
+
+            <div className="header__wrap">
+                <textarea
+                    className="header__input"
+                    placeholder="Search"
+                    name="input"
+                ></textarea>
+                
+                <img className="header__img"
+                    src={mohanImg}
+                    alt="user image"
+                />
+
+                {/* <button className="header__btn"> upload </button> */}
+            </div>
+            <button className="header__btn"> upload </button>
+        </header>
+        </>
+    )
+}
+export default Header;
