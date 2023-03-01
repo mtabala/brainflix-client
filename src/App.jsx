f140afa52ce6a9e0d09c4de0a96a17294f06ab03
Masha <mariia.tabala@gmail.com>
2023-02-28 22:06:53 -0800

    Create a draft for the Video section in jsx and scss.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
 import Header from "./components/Header/Header"
+import HeroVideo from "./components/HeroVideo/HeroVideo"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HeroVideo />
     </div>
   );
 }
---
+++ src/components/HeroVideo/HeroVideo.jsx
@@ -0,0 +1,12 @@
+import "./HeroVideo.scss"
+
+function HeroVideo () {
+    return (
+        <>
+        <section className="hero">
+            <video controls className="hero__video" poster="#" src="#"></video>
+        </section>
+        </>
+    )
+}
+export default HeroVideo;
