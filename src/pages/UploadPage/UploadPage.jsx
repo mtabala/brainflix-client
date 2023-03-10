56e03c4c680b47c878890c5b4aa65009de13a32b
Masha <mariia.tabala@gmail.com>
2023-03-09 19:27:49 -0800

    Remove the bottom border from the last child in the comments.Fix the upload page remove extra data.

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,24 +1,10 @@
-import UploadImg from "../../assets/Images/Upload-video-preview.jpg";
 import UploadForm from "../../components/UploadForm/UploadForm";
 import "../UploadPage/UploadPage.scss";
-import {Link, useNavigate } from "react-router-dom";
 function UploadPage() {
-    // const navigate = useNavigate ()
-    // const alertUpload = () => {
-    //     alert("Thank you for submitting your video")
-    //     navigate("/")
-    // }
     return (
         <section className="upload">
-            <h1 className="upload__header">Upload Video</h1>
-                
-                    <UploadForm />
-
-                {/* <div className="upload__btns">
-                    <button className="btn btn--publish" to="/" onClick={ alertUpload}>publish</button>
-                    <Link className="btn btn--cancel" to="/">cancel</Link>
-                </div> */}
-
+            <h1 className="upload__header">Upload Video</h1>                
+            <UploadForm />
         </section>
     );
 }
