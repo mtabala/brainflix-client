d90ef0bb480ed42dd64477c22cc230a76cf7d14c
Masha <mariia.tabala@gmail.com>
2023-03-09 16:35:08 -0800

    Fix the upload page jsx and scss wise. Fix the scaling and hovering for the video list card animation.

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,15 +1,35 @@
+import UploadImg from "../../assets/Images/Upload-video-preview.jpg";
 import "../UploadForm/UploadForm.scss";
+import {Link, useNavigate } from "react-router-dom";
 
 function UploadForm() {
+    const navigate = useNavigate ()
+        const alertUpload = () => {
+        alert("Thank you for submitting your video")
+        navigate("/")
+    }
 
     return (
         <>
         <form className="upload__form">
-            <label htmlFor="title" className="upload__label">title your video</label>
-            <input id="title" className="upload__input" placeholder="Add a title to your video"></input>
+            <div className="upload__form-wrapper">
+                <div className="upload__thumbnail">
+                    <p className="upload__title">video thumbnail</p>
+                    <img className="upload__img" src={UploadImg} alt="thumbnail img"/>
+                </div>
+                
+                <div className="upload__wrapper">
+                    <label htmlFor="title" className="upload__label">title your video</label>
+                    <input id="title" className="upload__input" placeholder="Add a title to your video"></input>
+                    <label htmlFor="description" className="upload__label">add a video description</label>
+                    <textarea id="description" className="upload__input upload__input--description" placeholder="Add a description to your video"></textarea>
+                </div>
+            </div>
 
-            <label htmlFor="description" className="upload__label">add a video description</label>
-            <textarea id="description" className="upload__input upload__input--description" placeholder="Add a description to your video"></textarea>
+            <div className="upload__btns">
+                <button className="btn btn--publish" to="/" onClick={ alertUpload}>publish</button>
+                <Link className="btn btn--cancel" to="/">cancel</Link>
+            </div>      
         </form>
         </>
     );
--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -3,29 +3,21 @@ import UploadForm from "../../components/UploadForm/UploadForm";
 import "../UploadPage/UploadPage.scss";
 import {Link, useNavigate } from "react-router-dom";
 function UploadPage() {
-    const navigate = useNavigate ()
-    const alertUpload = () => {
-        alert("Thank you for submitting your video")
-        navigate("/")
-    }
+    // const navigate = useNavigate ()
+    // const alertUpload = () => {
+    //     alert("Thank you for submitting your video")
+    //     navigate("/")
+    // }
     return (
         <section className="upload">
             <h1 className="upload__header">Upload Video</h1>
-
-            <article className="upload__container">
-                <div className="upload__wrap">
-                    <div className="upload__thumbnail">
-                        <p className="upload__title">video thumbnail</p>
-                        <img className="upload__img" src={UploadImg} alt="thumbnail img"/>
-                    </div>
+                
                     <UploadForm />
-                </div>
 
-                <div className="upload__btns">
+                {/* <div className="upload__btns">
                     <button className="btn btn--publish" to="/" onClick={ alertUpload}>publish</button>
                     <Link className="btn btn--cancel" to="/">cancel</Link>
-                </div>
-            </article>
+                </div> */}
 
         </section>
     );
